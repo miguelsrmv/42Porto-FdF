962489e5a173716f16292ee542ca3e3e089efcd9
Miguel <miguel.srmv@gmail.com>
2023-09-10 00:04:19 +0100

    Cleanup

--- src/helper.c
+++ src/helper.c
@@ -6,7 +6,7 @@
 /*   By: mde-sa-- <mde-sa--@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/09 18:09:45 by mde-sa--          #+#    #+#             */
-/*   Updated: 2023/09/09 23:04:33 by mde-sa--         ###   ########.fr       */
+/*   Updated: 2023/09/10 00:04:00 by mde-sa--         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,9 @@
 
 void    print_buffered_data(t_pixel *pixel_data, t_map_data map_data)
 {
-	ft_printf("x\t\ty\t\tz\t\thex\n");
-    ft_printf("_________________________________________________________\n");
+	ft_printf("\n\tx\t\ty\t\tz\t\thex\n");
+    ft_printf("_________________________________________________________________\n");
 	for (int i = 0; i < (map_data.x * map_data.y); i++)
-	{
-		ft_printf("%i\t\t%i\t\t%i\t\t0x%x\n", pixel_data[i].x, pixel_data[i].y, pixel_data[i].z, pixel_data[i].color);
-	}
+		ft_printf("\t%i\t\t%i\t\t%i\t\t0x%x\n", pixel_data[i].x, pixel_data[i].y, pixel_data[i].z, pixel_data[i].color);
+	ft_printf("_________________________________________________________________\n");
 }
